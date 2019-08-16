727b10d2d38e730a5013e9260ee4f356f1244dec
CeeDeeBee <cdbarnes98@gmail.com>
2019-08-15 21:30:56 -0400

    Added authentication

--- flask-framework.py
+++ flask-framework.py
@@ -1,12 +1,20 @@
 import os
 from flask import Flask, render_template
+from flask_httpauth import HTTPBasicAuth
+from passlib.hash import sha256_crypt
 
-template_dir = os.path.abspath('/')
 app = Flask(__name__)
+auth = HTTPBasicAuth()
 
-@app.route('/')
+#password verification
+@auth.verify_password
+def verify_password(username, password):
+    return sha256_crypt.verify(password, '$5$rounds=535000$ym4ec2WzY/sqzuVK$OqqWwlHE3WYBLe5ERGDlz7k.ZNglHPOaxUYuQxata8C')
+
+@app.route('/test-portfolio')
+@auth.login_required
 def index():
-    return render_template('index.html', template_folder=template_dir)
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
