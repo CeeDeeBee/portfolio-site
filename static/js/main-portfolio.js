92248d0e2d5de42a134214cb074bcc0c7487bb1c
CeeDeeBee <cdbarnes98@gmail.com>
2019-10-10 20:45:22 -0400

    Implemented maker page descriptions and links

--- static/js/main-portfolio.js
+++ static/js/main-portfolio.js
@@ -5,7 +5,17 @@ $(document).ready(function() {
         $('.entrepreneur-div, .writer-div').hide();
         $('.main-container').css('justify-content', 'flex-end');
         $('.maker-container').removeClass('hidden');
+        $('.maker').removeClass('clickable');
         $('.name').data('page', 'maker');
+    });
+    $('.maker-card').click(function() {
+        //Open the site affiliated with the maker card
+        let site = $(this).data('site');
+        if (site && site != './classic') {
+            window.open(site, '_blank');
+        } else {
+            window.open(site, '_self');
+        }
     });
     $('.name').click(function() {
         //When name is clicked open about page if current page is main or open main page if current page is not main
@@ -19,6 +29,7 @@ $(document).ready(function() {
             //Show main page
             $('.main-container').css('justify-content', 'center');
             $('.menu-div').show();
+            $('.menu-item').addClass('clickable');
             $('.name').data('page', 'main');
         }
     });
