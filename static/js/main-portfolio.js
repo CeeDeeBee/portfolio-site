78ac40d8c6247604f71c8a0f1e98884384fe148a
CeeDeeBee <cdbarnes98@gmail.com>
2019-10-15 22:56:07 -0400

    Began Implementing About Page

--- static/js/main-portfolio.js
+++ static/js/main-portfolio.js
@@ -1,13 +1,18 @@
 //On DOM Load
 $(document).ready(function() {
-    init();
+    let isMakerPageInit = false;
+    let isAboutPageInit = false; 
+
     $('.maker').click(function() {
         //Open maker page when maker button is clicked
+        if (!isMakerPageInit) {
+            isMakerPageInit = makerPageInit();
+        }
         $('.entrepreneur-div, .writer-div').hide();
         $('.main-container').css('justify-content', 'flex-end');
-        $('.maker-container').removeClass('hidden');
         $('.maker').removeClass('clickable');
         $('.name').data('page', 'maker');
+        $('.maker-container').removeClass('hidden');
     });
     $('.maker-container').on('click', '.maker-card-title', function() {
         //Open the site affiliated with the maker card
@@ -22,8 +27,12 @@ $(document).ready(function() {
         //When name is clicked open about page if current page is main or open main page if current page is not main
         let data = $('.name').data('page');
         if (data == 'main') {
+            if (!isAboutPageInit) {
+                isAboutPageInit = aboutPageInit();
+            }
             $('.menu-div').hide();
             $('.name').data('page', 'about');
+            $('.about-container').removeClass('hidden');
         } else {
             //Hide current content 
             $('.' + data + '-container').addClass('hidden');
@@ -36,29 +45,30 @@ $(document).ready(function() {
     });
 });
 
-function init() {
+function makerPageInit() {
     //Initialize Maker Page Cards With JSON
-    $.getJSON('/static/maker-cards.json', function(data) {
+    $.getJSON('/static/data.json', function(data) {
+        let makerPageData = data.makerCards
         let makerContainer = $('.maker-container');
         let makerCardTemplate = $('#maker-card-template');
 
-        for (var i = 0; i < data.length; i ++) {
-            makerCardTemplate.find('img').attr('src', "/static/images/" + data[i].image);
+        for (let i = 0; i < makerPageData.length; i ++) {
+            makerCardTemplate.find('img').prop('src', "/static/images/" + makerPageData[i].image);
             let h1 = makerCardTemplate.find('h1');
-            if (data[i].site != null) {
+            if (makerPageData[i].site) {
                 h1.addClass('maker-card-title clickable');
-                h1.attr('data-site', data[i].site);
+                h1.prop('data-site', makerPageData[i].site);
             } else {
                 h1.removeClass();
                 h1.removeAttr('data-site')
             }
-            h1.text(data[i].title);
+            h1.text(makerPageData[i].title);
             let pTags = makerCardTemplate.find('p');
-            $(pTags[0]).text(data[i].description);
-            $(pTags[1]).text(data[i].tools);
+            $(pTags[0]).text(makerPageData[i].description);
+            $(pTags[1]).text(makerPageData[i].tools);
             let makerCardA = makerCardTemplate.find('a');
-            if (data[i].github != null) {
-               makerCardA.prop({'href': data[i].github, 'target': '_blank'}).text('GitHub');
+            if (makerPageData[i].github != null) {
+               makerCardA.prop({'href': makerPageData[i].github, 'target': '_blank'}).text('GitHub');
             } else {
                 makerCardA.empty().removeAttr('href target');
             }
@@ -66,4 +76,25 @@ function init() {
             makerContainer.append(makerCardTemplate.html());
         }
     });
+
+    return true;
+}
+
+function aboutPageInit() {
+    //Initialize About Page With JSON
+    $.getJSON('/static/data.json', function(data) {
+        let aboutPageData = data.about;
+        let aboutContainer = $('.about-container');
+
+        aboutContainer.find('p').text(aboutPageData.description);
+        aboutContainer.find('a').prop('href', aboutPageData.resume).text('Resume');
+        let aboutDiv = aboutContainer.find('div');
+        console.log(aboutPageData);
+        for (let i = 0; i < aboutPageData.socialLinks.length; i ++) {
+            aboutDiv.append($('<a></a>').prop({'href': '/static/' + aboutPageData.socialLinks[i].link, 'target': '_blank'}));
+            aboutDiv.append($('<img>').prop({'src': '/static/images/' + aboutPageData.socialLinks[i].img, 'height': '200px'}));
+        }
+    });
+
+    return true;
 }
