9f395e950f0d8d842299cc732c8acdbd4859f81b
CeeDeeBee <cdbarnes98@gmail.com>
2019-10-23 17:04:43 -0400

    Finished Implementing Writer Page

--- static/js/main-portfolio.js
+++ static/js/main-portfolio.js
@@ -2,6 +2,7 @@
 $(document).ready(function() {
     let isMakerPageInit = false;
     let isAboutPageInit = false; 
+    let isWriterPageInit = false;
 
     $('.maker').click(function() {
         //Open maker page when maker button is clicked
@@ -24,11 +25,18 @@ $(document).ready(function() {
         }
     });
     $('.writer').click(function() {
+        if (!isWriterPageInit) {
+            isWriterPageInit = writerPageInit();
+        }
         $('.entrepreneur-div, .maker-div').hide();
         $('.main-container').css('justify-content', 'flex-end');
         $('.writer').removeClass('clickable');
         $('.name').data('page', 'writer');
         $('.writer-container').removeClass('hidden');
+    });
+    $('.writer-container').on('click', '.writer-card-title', function() {
+        //Open the page affiliated with the writer card
+        window.open($(this).data('content'), '_blank');
     });
     $('.name').click(function() {
         //When name is clicked open about page if current page is main or open main page if current page is not main
@@ -59,26 +67,27 @@ function makerPageInit() {
         let makerPageData = data.makerCards
         let makerContainer = $('.maker-container');
         let makerCardTemplate = $('#maker-card-template');
+        let img = makerCardTemplate.find('img');
+        let h1 = makerCardTemplate.find('h1');
+        let p = makerCardTemplate.find('p');
+        let a = makerCardTemplate.find('a');
 
         for (let i = 0; i < makerPageData.length; i ++) {
-            makerCardTemplate.find('img').prop('src', "/static/images/" + makerPageData[i].image);
-            let h1 = makerCardTemplate.find('h1');
+            img.prop('src', "/static/images/" + makerPageData[i].image);
             if (makerPageData[i].site) {
-                h1.addClass('maker-card-title clickable');
+                h1.addClass('clickable');
                 h1.attr('data-site', makerPageData[i].site);
             } else {
                 h1.removeClass();
                 h1.removeAttr('data-site');
             }
             h1.text(makerPageData[i].title);
-            let pTags = makerCardTemplate.find('p');
-            $(pTags[0]).text(makerPageData[i].description);
-            $(pTags[1]).text(makerPageData[i].tools);
-            let makerCardA = makerCardTemplate.find('a');
+            $(p[0]).text(makerPageData[i].description);
+            $(p[1]).text(makerPageData[i].tools);
             if (makerPageData[i].github) {
-               makerCardA.prop({'href': makerPageData[i].github, 'target': '_blank'}).text('GitHub');
+                a.prop({'href': makerPageData[i].github, 'target': '_blank'}).text('GitHub');
             } else {
-                makerCardA.empty().removeAttr('href target');
+                a.empty().removeAttr('href target');
             }
 
             makerContainer.append(makerCardTemplate.html());
@@ -100,6 +109,27 @@ function aboutPageInit() {
         for (let i = 0; i < aboutPageData.socialLinks.length; i ++) {
             aboutDiv.append($('<a></a>').prop({'href': aboutPageData.socialLinks[i].link, 'target': '_blank'}));
             $(aboutDiv.find('a')[i]).append($('<img>').prop('src', '/static/images/' + aboutPageData.socialLinks[i].img).addClass('about-img'));
+        }
+    });
+
+    return true;
+}
+
+function writerPageInit() {
+    //Initialize Writer Page With JSON
+    $.getJSON('/static/data.json', function(data) {
+        let writerPageData = data.writer;
+        let writerContainer = $('.writer-container');
+        let writerCardTemplate = $('#writer-card-template');
+        let h1 = writerCardTemplate.find('h1');
+        let p = writerCardTemplate.find('p');
+
+        for (let i = 0; i < writerPageData.length; i ++) {
+            h1.text(writerPageData[i].title)
+            h1.attr('data-content', writerPageData[i].contentLocation);
+            p.text(writerPageData[i].synopsis);
+
+            writerContainer.append(writerCardTemplate.html());
         }
     });
 
