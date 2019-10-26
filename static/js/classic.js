f3999154820535e75cca1cb17b1e29c22a3cc669
CeeDeeBee <cdbarnes98@gmail.com>
2019-10-26 17:26:56 -0400

    Fixed Mobile Formatting for Classic + Implemented Mobile Text Input

--- static/js/classic.js
+++ static/js/classic.js
@@ -1,33 +1,41 @@
 const cbarnesOsStr = `
- /$$$$$$$$ /$$                
-|__  $$__/| $$                
-   | $$   | $$$$$$$   /$$$$$$ 
-   | $$   | $$__  $$ /$$__  $$
-   | $$   | $$  \\ $$| $$$$$$$$
-   | $$   | $$  | $$| $$_____/
-   | $$   | $$  | $$|  $$$$$$$
-   |__/   |__/  |__/ \\_______/
+                                                                            
+                      /$$$$$$$$ /$$                
+                     |__  $$__/| $$                
+                        | $$   | $$$$$$$   /$$$$$$ 
+                        | $$   | $$__  $$ /$$__  $$
+                        | $$   | $$  \\ $$| $$$$$$$$
+                        | $$   | $$  | $$| $$_____/
+                        | $$   | $$  | $$|  $$$$$$$
+                        |__/   |__/  |__/ \\_______/
                              
                              
-                                      /$$                                                            
-                                     | $$                                                            
-                             /$$$$$$$| $$$$$$$   /$$$$$$   /$$$$$$  /$$$$$$$       /$$$$$$   /$$$$$$$
-                            /$$_____/| $$__  $$ |____  $$ /$$__  $$| $$__  $$     /$$__  $$ /$$_____/
-                           | $$      | $$  \\ $$  /$$$$$$$| $$  \\__/| $$  \\ $$    | $$$$$$$$|  $$$$$$ 
-                           | $$      | $$  | $$ /$$__  $$| $$      | $$  | $$    | $$_____/ \\____  $$
-                           |  $$$$$$$| $$$$$$$/|  $$$$$$$| $$      | $$  | $$ /$$|  $$$$$$$ /$$$$$$$/
-                            \\_______/|_______/  \\_______/|__/      |__/  |__/|__/ \\_______/|_______/ 
-                                                                                                                                              
+           /$$                                                            
+          | $$                                                            
+  /$$$$$$$| $$$$$$$   /$$$$$$   /$$$$$$  /$$$$$$$       /$$$$$$   /$$$$$$$
+ /$$_____/| $$__  $$ |____  $$ /$$__  $$| $$__  $$     /$$__  $$ /$$_____/
+| $$      | $$  \\ $$  /$$$$$$$| $$  \\__/| $$  \\ $$    | $$$$$$$$|  $$$$$$
+| $$      | $$  | $$ /$$__  $$| $$      | $$  | $$    | $$_____/ \\____  $$
+|  $$$$$$$| $$$$$$$/|  $$$$$$$| $$      | $$  | $$ /$$|  $$$$$$$ /$$$$$$$/
+ \\_______/|_______/  \\_______/|__/      |__/  |__/|__/ \\_______/|_______/
 
-                                                                                                     /$$$$$$   /$$$$$$ 
-                                                                                                    /$$__  $$ /$$__  $$
-                                                                                                   | $$  \\ $$| $$  \\__/
-                                                                                                   | $$  | $$|  $$$$$$ 
-                                                                                                   | $$  | $$ \\____  $$
-                                                                                                   | $$  | $$ /$$  \\ $$
-                                                                                                   |  $$$$$$/|  $$$$$$/
-                                                                                                    \\______/  \\______/ 
+
+
+                            /$$$$$$   /$$$$$$ 
+                           /$$__  $$ /$$__  $$
+                          | $$  \\ $$| $$  \\__/
+                          | $$  | $$|  $$$$$$ 
+                          | $$  | $$ \\____  $$
+                          | $$  | $$ /$$  \\ $$
+                          |  $$$$$$/|  $$$$$$/
+                           \\______/  \\______/ 
                    `
+const osInfo1 = `The CBarn.es Networked BOS
+Version 1.00 `
+const osInfo2 = `(C)Copyright CBarn.es Corp 2019
+
+Enter "help" To List Commands`
+
 const descriptions = `Commands:
 Reboot - Reboots the OS
 Help - Prints this dialogue
@@ -35,15 +43,18 @@ Boring - Go back to the boring site`
 var booted = false;
 //On DOM load
 $(document).ready(function() {
-    $('body').click(function () { $('body').focus(); });
+    $(document).click(function (e) { 
+        $('.forceKbInput').focus(); 
+    });
 	cursorInterval = bootSequence();
 	const commands = {
         'reboot': function() {
             booted = false;
             $(document.body).append($('#cursor').empty());
             clearInterval(cursorInterval);
+            $('#osInfo').empty();
             $('.line').remove();
-            $('#bootText').css('display', 'block');
+            $('#bootText').css('display', 'block').after($('#cursor'));
             $('#cbarnesOsStr').css('display', 'block');
             cursorInterval = bootSequence();
         },
@@ -58,7 +69,7 @@ $(document).ready(function() {
             location.href = '/test-portfolio';
         }
     };
-	//Look for keypresses
+	//Listen for keypresses
 	$(document).on('keydown', function(e) {
 		if (booted) {
             //console.log(e.keyCode);
@@ -76,70 +87,10 @@ $(document).ready(function() {
                 }
 				newLine();
 			} else if (e.keyCode != 16) {
-				$('.currentInput').append(e.key);
+				$('.currentInput').text($('.currentInput').text() + e.key);
 			}
 		}
 	});
-    /*
-    var canvas = document.getElementById("canvas");
-    var ctx = canvas.getContext("2d");    
-    window.onresize = initCanvas;
-
-    var frame = 1;
-    var cursorX = 0;
-    var cursorY = 0;
-    finishedBoot = false;
-    var pointerY = 35;
-    var currentEntry = [];
-    initCanvas();
-
-    //handle keystrokes
-    $(document).on("keydown", function(e) {
-        if (finishedBoot) {
-            //ßconsole.log(e);
-            //if user pushes backspace
-            if (e.keyCode == 8) {
-                if (cursorX > 122) {
-                    //draw background color over area to write letter
-                    ctx.fillStyle = '#282828';
-                    ctx.fillRect(cursorX, cursorY, 100, 15);
-                    //update cursor location
-                    if (cursorX - keyWidth < 122) {
-                        cursorX = 122;
-                    } else {
-                        cursorX -= ctx.measureText(currentEntry.pop()).width;
-                    }
-                }
-            } else if (e.keyCode == 13) {
-                if (currentEntry.join('') == 'home') {
-                    $('#headshot').show();
-                    ctx.drawImage(document.getElementById('headshot'), 100, 100);
-                }
-                //draw background color over area to write letter
-                ctx.fillStyle = '##282828';
-                ctx.fillRect(cursorX, cursorY, 10, 15);
-                //update cursor and pointer locations
-                pointerY += 20;
-                cursorX = 122;
-                cursorY += 20;
-                drawPointer();
-            } else if (e.keyCode != 20 && e.keyCode != 16 && e.keyCode != 9 && e.keyCode != 17 && e.keyCode != 18 && e.keyCode != 91){
-                //get width of entered key
-                keyWidth = ctx.measureText(e.key).width;
-                //draw background color over area to write letter
-                ctx.fillStyle = '#282828';
-                ctx.fillRect(cursorX, cursorY, 10, 15);
-                //draw letter
-                ctx.font = '12px IBMPC';
-                ctx.fillStyle = '#33ff33';
-                ctx.fillText(e.key, cursorX, cursorY + 12);
-                //update cursor location
-                cursorX += keyWidth;
-                currentEntry.push(e.key);
-            }
-        }
-    });
-    */
 });
 
 /**
@@ -182,17 +133,26 @@ async function bootSequence() {
 		$('#bootText').append(bootTextStr[chr]);
 		await sleep(0.25);
     }
-	cursorInterval = cursor();
+    cursorInterval = cursor();
     await sleep(3000);
+    $('#cursor').hide();
     //Clear boot screen
 	$('#bootText').empty().css('display', 'none');
     $('#cbarnesOsStr').empty().css('display', 'none');
     //Setup terminal interface
-    $('#bootText').after($('<div></div>').addClass('line'));
+    for (let chr in osInfo1) {
+        $('#osInfo').append(osInfo1[chr]);
+        await sleep(0.25);
+    }
+    $('#osInfo').append('<br>');
+    for (let chr in osInfo2) {
+        $('#osInfo').append(osInfo2[chr]);
+        await sleep(0.25);
+    }
+    $('#shell').append($('<div></div>').addClass('line'));
     $('.line').append($('<div></div>').html('A>').addClass('pointer currentPointer'));
 	$('.line').append($('<div></div>').addClass('input currentInput'));
-    $('.currentPointer').html('A>')
-    $('.line').append($('#cursor'));
+    $('.line').append($('#cursor').show());
     booted = true;
     
     return cursorInterval;
@@ -210,7 +170,9 @@ function newLine() {
 	$('.line').last().append($('<div></div>').html('A>').addClass('pointer currentPointer'));
 	$('.line').last().append($('<div></div>').addClass('input currentInput'));
 	//Move cursor into new line div
-	$('.line').last().append($('#cursor'));
+    $('.line').last().append($('#cursor'));
+    //Scroll to bottom of page
+    $('#shell').scrollTop($('#shell')[0].scrollHeight);
 }
 /**
  * Print if user enters invalid command
